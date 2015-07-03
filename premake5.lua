e2503ee44c2a63312ef65bebc9b98888b0a5cfcf
Philpax <me@philpax.me>
2015-07-03 19:32:00 +1000

    Fixed #2 and #3 - replaced solution with premake project, removed Afterdawn

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+VENDOR_INCLUDES = { "vendor/glm", "vendor/SDL2-2.0.3/include" }
+
+project "Vespertine"
+	kind "StaticLib"
+	language "C++"
+	targetdir "lib/%{cfg.buildcfg}"
+
+	includedirs(VENDOR_INCLUDES)
+	includedirs { "include/" }
+	files { "include/**.hpp", "src/**.cpp" }
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
+	configuration { "gmake" }
+		buildoptions { "-std=c++11" }
