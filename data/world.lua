0a97a8bfe4e2c6612b55f71fc009eb42d37b88ec
Philpax <me@philpax.me>
2016-07-27 23:55:04 +1000

    World: Procedurally generate a new building on a regular basis

--- data/world.lua
+++ data/world.lua
@@ -1,38 +1,60 @@
 dofile "interface.lua"
 dofile "primitives.lua"
 
-local verts = {}
-local origin = Vec3(200, 58, 450)
-local windowCount = 5
-local wallHeight = 3
-local cellWidth = 3
-local thickness = 0.15
-local levelCount = 10
-local windowSize = Vec2(2, 2.2)
+math.randomseed(os.time())
 
-for level = 0, levelCount-1 do
-    local levelOrigin = origin + Vec3(0, wallHeight * level, 0)
-    Cuboid(verts, levelOrigin + Vec3(thickness, 0, thickness), Vec3(windowCount*cellWidth - 2*thickness, 0.1, windowCount*cellWidth - 2*thickness), Colour(80, 80, 80, 255))
-
-    for i = 0, windowCount-1 do
-        -- Front
-        WindowWallCell(verts, levelOrigin + Vec3(cellWidth*i, 0, 0), Vec3(cellWidth, wallHeight, thickness), Colour(120, 120, 120, 255), windowSize, false)
-        -- Back
-        WindowWallCell(verts, levelOrigin + Vec3(cellWidth*i, 0, windowCount*cellWidth - thickness), Vec3(cellWidth, wallHeight, thickness), Colour(120, 120, 120, 255), windowSize, false)
-        -- Left
-        WindowWallCell(verts, levelOrigin + Vec3(0, 0, cellWidth*i), Vec3(cellWidth, wallHeight, thickness), Colour(120, 120, 120, 255), windowSize, true)
-        -- Right
-        WindowWallCell(verts, levelOrigin + Vec3(windowCount*cellWidth - thickness, 0, cellWidth*i), Vec3(cellWidth, wallHeight, thickness), Colour(120, 120, 120, 255), windowSize, true)
+lastBuilding = nil
+function MakeBuilding()
+    if lastBuilding ~= nil then
+        mesh.remove(lastBuilding)
     end
+
+    local verts = {}
+    local origin = Vec3(200, 58, 450)
+    local windowCount = math.random(3, 7)
+    local wallHeight = 3
+    local cellWidth = math.random(3, 6)
+    local thickness = 0.15
+    local levelCount = math.random(5, 15)
+    local windowWidth = math.randomrange(1.5, 2.5)
+    local windowSize = Vec2(windowWidth, windowWidth + 0.2)
+
+    for level = 0, levelCount-1 do
+        local levelOrigin = origin + Vec3(0, wallHeight * level, 0)
+        Cuboid(verts, levelOrigin + Vec3(thickness, 0, thickness), Vec3(windowCount*cellWidth - 2*thickness, 0.1, windowCount*cellWidth - 2*thickness), Colour(80, 80, 80, 255))
+
+        local colourScale = math.lerp(80, 120, (level+1)/levelCount)
+        local colour = Colour(colourScale, colourScale, colourScale, 255)
+        for i = 0, windowCount-1 do
+            -- Front
+            WindowWallCell(verts, levelOrigin + Vec3(cellWidth*i, 0, 0), Vec3(cellWidth, wallHeight, thickness), colour, windowSize, false)
+            -- Back
+            WindowWallCell(verts, levelOrigin + Vec3(cellWidth*i, 0, windowCount*cellWidth - thickness), Vec3(cellWidth, wallHeight, thickness), colour, windowSize, false)
+            -- Left
+            WindowWallCell(verts, levelOrigin + Vec3(0, 0, cellWidth*i), Vec3(cellWidth, wallHeight, thickness), colour, windowSize, true)
+            -- Right
+            WindowWallCell(verts, levelOrigin + Vec3(windowCount*cellWidth - thickness, 0, cellWidth*i), Vec3(cellWidth, wallHeight, thickness), colour, windowSize, true)
+        end
+    end
+
+    -- Final ceiling
+    Cuboid(verts, origin + Vec3(0, levelCount*wallHeight, 0), Vec3(windowCount*cellWidth, 0.1, windowCount*cellWidth), Colour(80, 80, 80, 255))
+    -- Corner pillars
+    local pillarSize = 0.5
+    Cuboid(verts, origin + Vec3(thickness, 0, thickness), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
+    Cuboid(verts, origin + Vec3(windowCount*cellWidth - (pillarSize + thickness), 0, thickness), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
+    Cuboid(verts, origin + Vec3(thickness, 0, windowCount*cellWidth - (pillarSize + thickness)), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
+    Cuboid(verts, origin + Vec3(windowCount*cellWidth - (pillarSize + thickness), 0, windowCount*cellWidth - (pillarSize + thickness)), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
+
+    lastBuilding = mesh.add(verts)
+    -- print("New building!")
 end
 
--- Final ceiling
-Cuboid(verts, origin + Vec3(0, levelCount*wallHeight, 0), Vec3(windowCount*cellWidth, 0.1, windowCount*cellWidth), Colour(80, 80, 80, 255))
--- Corner pillars
-local pillarSize = 0.5
-Cuboid(verts, origin + Vec3(thickness, 0, thickness), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
-Cuboid(verts, origin + Vec3(windowCount*cellWidth - (pillarSize + thickness), 0, thickness), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
-Cuboid(verts, origin + Vec3(thickness, 0, windowCount*cellWidth - (pillarSize + thickness)), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
-Cuboid(verts, origin + Vec3(windowCount*cellWidth - (pillarSize + thickness), 0, windowCount*cellWidth - (pillarSize + thickness)), Vec3(pillarSize, levelCount*wallHeight, pillarSize), Colour(60, 60, 60, 255))
-
-mesh.add(verts)
+last = os.clock()
+MakeBuilding()
+function pulse()
+    if os.clock() - last > 5 then
+        MakeBuilding()
+        last = os.clock()
+    end
+end
